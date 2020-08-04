5add8181ac5685e49e035954c012d59ea7ef83e8
Sarnyai Akos <sarnyaia1@gmail.com>
2020-08-04 18:15:50 +0200

    made the site interactive

---
+++ app.js
@@ -0,0 +1,7 @@
+function change_page(){
+    // similar behavior as an HTTP redirect
+    window.location.replace("test.html");
+    //or
+    // similar behavior as clicking on a link
+    window.location.href = "test.html";
+ };
