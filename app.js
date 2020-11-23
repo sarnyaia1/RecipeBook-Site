870a58b8c6b3cd121ea13b8dc2efe9b8ef532802
Sarnyai Akos <sarnyaia1@gmail.com>
2020-11-23 22:37:36 +0100

    add a new recipe
    Broccoli Cheddar cream soup site

--- app.js
+++
@@ -1,7 +0,0 @@
-function change_page(){
-    // similar behavior as an HTTP redirect
-    window.location.replace("test.html");
-    //or
-    // similar behavior as clicking on a link
-    window.location.href = "test.html";
- };
